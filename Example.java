a17d1b3715ae711a8ae0805b04dffd25e22c989d
vanessamaldonado <vmaldonado@iessonferrer.net>
2025-12-04 11:39:09 +0100

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,10 @@
+/** 
+ * example of a java program for the Apasoft Training Jenkins course
+ */
+
+public class Example {
+  public static void main(String[] args) {
+    System.out.println("This is an example of a java program");
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Practica 03  Compilar y ejecutar un programa en JAVA
+Ficheros practica 03: Compilar y ejecutar un programa en JAVA.
